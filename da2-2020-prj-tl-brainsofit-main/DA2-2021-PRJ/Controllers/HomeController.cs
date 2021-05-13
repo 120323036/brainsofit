f8f75d9dfc73f4baf62bc8c6be3154ddc6d8896d
Emilio <Emilio@EMILIO-PC>
2021-05-13 19:44:21 +0100

    jus

---
+++ da2-2020-prj-tl-brainsofit-main/DA2-2021-PRJ/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using DA2_2021_PRJ.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DA2_2021_PRJ.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Acerca()
+        {
+            return View();
+        }
+
+        public IActionResult Equipa()
+        {
+            return View();
+        }
+
+        public IActionResult InfoPessoalRodrigo()
+        {
+            return View();
+        }
+
+        public IActionResult InfoPessoalWalkiria()
+        {
+            return View();
+        }
+
+        public IActionResult InfoPessoalJustino()
+        {
+            return View();
+        }
+        public IActionResult InfoPessoalEvani()
+        {
+            return View();
+        }
+
+        public IActionResult InfoPessoalIsabel()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ da2-2020-prj-tl-brainsofit-main/DA2-2021-PRJ/Controllers/biblioteca.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DA2_2021_PRJ.Controllers
+{
+    public class biblioteca : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
